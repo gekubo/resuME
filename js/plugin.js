420b23ccd2c9b87451047095f161364d41a861b8
Guillermo García Galindo <gekubo@me.com>
2018-02-04 23:08:03 +0100

    Version 0.2
    Added Flexbox and CSS Grid

--- js/plugin.js
+++ js/plugin.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
   $(".setsize").each(function() {
       $(this).height($(this).width());
   });
-/*  window.shuffleInstance = new window.Shuffle(document.getElementById('grid'), {
-  itemSelector: '.grid_brick',
-  sizer: '.my-sizer-element',
-  });*/
 
   $(window).on("resize", function(){
     $(".setsize").each(function() {
@@ -39,9 +35,9 @@ $(document).ready(function() {
   window.onload = resizeAllGridItems();
   window.addEventListener("resize", resizeAllGridItems);
 
-  allItems = document.getElementsByClassName("item");
-  for(x=0;x<allItems.length;x++){
-    imagesLoaded( allItems[x], resizeInstance);
-  };
+  // allItems = document.getElementsByClassName("item");
+  // for(x=0;x<allItems.length;x++){
+  //   imagesLoaded( allItems[x], resizeInstance);
+  // };
 
 }); // End of document.ready
